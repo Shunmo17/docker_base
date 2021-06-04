ece99f5bdcfc3d86eac80e24602174a3161f99bd
Shunsuke <mochizushun@keio.jp>
2021-06-04 13:35:31 +0900

    WIP:

--- build.sh
+++ build.sh
@@ -2,3 +2,9 @@ COMPOSE_DOCKER_CLI_BUILD=1 \
 DOCKER_BUILDKIT=1 \
 docker-compose build \
     --parallel
+
+# COMPOSE_DOCKER_CLI_BUILD=1 \
+# DOCKER_BUILDKIT=1 \
+# docker-compose build \
+#     --no-cache \
+#     --parallel
